adc0c28cefa45dfd6900c592e96ef3ef603b467b
michalkwojtas <michalkwojtas@gmail.com>
2017-11-01 22:39:25 +0100

    Tags cloud font sizes, CSS metadata updated

--- wp-content/themes/nt-2017-theme/components/footer/site-info.php
+++ wp-content/themes/nt-2017-theme/components/footer/site-info.php
@@ -1,9 +1,9 @@
 <div class="tag-cloud">
 	<?php wp_tag_cloud( array(
-   'smallest' => 8, // size of least used tag
-   'largest'  => 22, // size of most used tag
+   'smallest' => 12, // size of least used tag
+   'largest'  => 40, // size of most used tag
    'unit'     => 'px', // unit for sizing the tags
-   'number'   => 45, // displays at most 45 tags
+   'number'   => 120, // displays at most 45 tags
    'orderby'  => 'name', // order tags alphabetically
    'order'    => 'ASC', // order tags by ascending order
    'taxonomy' => 'post_tag' // you can even make tags for custom taxonomies
