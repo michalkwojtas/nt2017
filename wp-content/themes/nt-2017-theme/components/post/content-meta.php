bdd1978190d26c2b6cf2b3c314d97bc1fdf4c650
michalkwojtas <michalkwojtas@gmail.com>
2017-11-14 08:36:48 +0100

    Displaying stars rating in post meta

--- wp-content/themes/nt-2017-theme/components/post/content-meta.php
+++ wp-content/themes/nt-2017-theme/components/post/content-meta.php
@@ -1,3 +1,8 @@
 		<div class="entry-meta">
-			<span class="cat-links"><?php the_category()?></span><span class="posted-on"><?php the_time("d.m.Y") ?></span>
+			<span class="cat-links"><?php the_category()?></span><span class="posted-on"><?php the_time("d.m.Y") ?></span><span class="match-rating">
+				<?php
+				$term = get_field('match-rating');
+				if( $term ): ?>
+				 <a class="match-rating" target="_blank" href="<?php echo get_term_link( $term ); ?>"><?php echo $term->name; ?></a>
+				<?php endif; ?></span>
 		</div><!-- .entry-meta -->
